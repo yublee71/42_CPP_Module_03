30689f9c1efd979d08e028cd8f48221fd33d2180
Yubeen Lee <lybin71@naver.com>
2025-07-15 00:23:15 +0100

    feat: ex00 done

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -1,9 +1,26 @@
 #ifndef CLAPTRAP_HPP
 #define CLAPTRAP_HPP
 
+#include <string>
+
 class ClapTrap {
 public:
+  ClapTrap();
+  ClapTrap(std::string name);
+  ClapTrap(const ClapTrap &other);
+  ~ClapTrap();
+
+  ClapTrap &operator=(const ClapTrap &other);
+
+  void attack(const std::string &target);
+  void takeDamage(unsigned int amount);
+  void beRepaired(unsigned int amount);
+
 private:
+  std::string name_;
+  unsigned int hit_points_;
+  unsigned int energy_points_;
+  unsigned int attack_damage_;
 };
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,3 +1,27 @@
 #include "ClapTrap.hpp"
 
-int main(void) {}
+#include <iostream>
+#include <string>
+
+int main(void) {
+  ClapTrap a;
+  a.attack("a random stranger");
+  a.takeDamage(5);
+  a.beRepaired(5);
+  a.attack("another random stranger");
+
+  ClapTrap b("Odin");
+  b.attack("a random stranger");
+  b.takeDamage(50);
+  b.beRepaired(15);
+  b.attack("another random stranger");
+
+  ClapTrap c(a);
+  c.attack("a random stranger");
+  for (int i = 0; i < 10; i++)
+    c.beRepaired(15);
+  c.attack("another random stranger");
+
+  ClapTrap d = c;
+  c.attack("a random stranger");
+}
